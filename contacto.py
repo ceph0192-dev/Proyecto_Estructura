9dfa8492b56c45220f6abe3be7b8b94815699c67
Ilse <ilsemartinezchimal@gmail.com>
2026-05-21 22:52:18 -0600

    Actualizando...

--- contacto.py
+++ contacto.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 
 class contacto:
-    def __init__(self, id, Nombres, Paterno, Materno,
+    def __init__(self, id_contacto, Nombres, Paterno, Materno,
 Direccion, Estado, Ciudad, Nacimiento, Edad, Tel, C_personal, 
 Matricula, C_institucional, Fac, Lic, F_ingreso,
 Antiguedad, F_registro, T_actualizacion):
@@ -43,6 +43,3 @@ Antiguedad, F_registro, T_actualizacion):
             meses += 12
         return f"{años} años y {meses} meses"
     
-    
-    
-
