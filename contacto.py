593a9be273346880cec0b846f5d8272f614907b5
Cristian <ceph0192@gmail.com>
2026-05-22 18:43:53 -0600

    Se agrego validación a el metodo de nuevo contato

--- contacto.py
+++ contacto.py
@@ -1,10 +1,10 @@
 from datetime import date, datetime
 
 class contacto:
-    def __init__(self, id_contacto, Nombres, Paterno, Materno,
+    def __init__(self, Nombres, Paterno, Materno,
                 Direccion, Estado, Ciudad, Nacimiento, Tel, C_personal, 
                 Matricula, C_institucional, Fac, Lic, F_ingreso):
-        
+
         self.__Matricula = Matricula
         self.__id = self.calcular_id()#Genera el id a partir de un metodo propio tipo hash
         self.__Nombres = Nombres
@@ -33,9 +33,9 @@ class contacto:
         for caracter in prox_id:
             valor_hash = (valor_hash * primo) + ord(caracter)
 
-            id = valor_hash % 100000000
+        id = valor_hash % 100000000
 
-            return str(id)
+        return str(id).zfill(8)
 
     def __calcular_edad(self):
         hoy = date.today()
@@ -43,7 +43,7 @@ class contacto:
         if (hoy.month, hoy.day) < (self.__Nacimiento.month, self.__Nacimiento.day):
             edad -= 1
         return edad
-    
+
     def __calcular_antiguedad(self):
         hoy = date.today()
         años = hoy.year - self.__F_ingreso.year
@@ -52,84 +52,84 @@ class contacto:
             años -= 1
             meses += 12
         return f"{años} años y {meses} meses"
-    
+
     # Getters para acceder a los atributos privados del contacto
     def get_id(self):
         return self.__id
-    
+
     def get_Nombres(self):
         return self.__Nombres
-    
+
     def get_Paterno(self):
         return self.__Paterno
-    
+
     def get_Materno(self):
         return self.__Materno
-    
+
     def get_nombre_completo(self):
         return f"{self.__Nombres} {self.__Paterno} {self.__Materno}"
-    
+
     def get_Direccion(self):
         return self.__Direccion
-    
+
     def get_Estado(self):
         return self.__Estado
-    
+
     def get_Ciudad(self):
         return self.__Ciudad
-    
+
     def get_Nacimiento(self):
         return self.__Nacimiento
-    
+
     def get_Edad(self): # Recalcula la edad siempre para qur sea precisa
         return self.__calcular_edad()
-    
+
     def get_Tel(self):
         return self.__Tel
-    
+
     def get_C_personal(self):
         return self.__C_personal
-    
+
     def get_Matricula(self):
         return self.__Matricula
-    
+
     def get_C_institucional(self):
         return self.__C_institucional
-    
+
     def get_Fac(self):
         return self.__Fac
-    
+
     def get_Lic(self):
         return self.__Lic
-    
+
     def get_F_ingreso(self):
         return self.__F_ingreso
-    
+
     def get_Antiguedad(self): # Recalcula la antigüedad siempre para que sea precisa
         return self.__calcular_antiguedad()
-    
+
     def get_F_registro(self):
         return self.__F_registro
-    
+
     def get_T_actualizacion(self):
         return self.__T_actualizacion
-    
+
     # Setters para modificar los atributos del contacto
     def set_Nombres(self, valor):
         self.__Nombres = valor 
-    
+
     def set_Paterno(self, valor):
         self.__Paterno = valor
-    
+
     def set_Materno(self, valor):
         self.__Materno = valor
-    
+
     def set_Direccion(self, valor):
         self.__Direccion = valor
-    
+
     def set_Estado(self, valor):
         self.__Estado = valor
-    
+
     def set_Ciudad(self, valor):
         self.__Ciudad = valor
 
@@ -139,28 +139,28 @@ class contacto:
 
     def set_Tel(self, valor):
         self.__Tel = valor
-    
+
     def set_C_personal(self, valor):
         self.__C_personal = valor
-    
+
     def set_Matricula(self, valor):
         self.__Matricula = valor
-    
+
     def set_C_institucional(self, valor):
         self.__C_institucional = valor
-    
+
     def set_Fac(self, valor):
         self.__Fac = valor
-    
+
     def set_Lic(self, valor):
         self.__Lic = valor
-    
+
     def set_F_ingreso(self, valor):
         self.__F_ingreso = valor
         self.__Antiguedad = self.__calcular_antiguedad() 
 
     def marcar_actualizacion(self):
         self.__T_actualizacion = datetime.now() # Registra la fecha y hora de la última actualización 
-    
+
     def mostrar_info(self):
         print("ID:", self.get_id())
--- metodos.py
+++ metodos.py
@@ -3,30 +3,128 @@ from contacto import contacto
 from datetime import datetime
 
 def nuevo_contacto(lista_contactos):
-    Nombres = input("Ingrese el nombre: ")
-    Paterno = input("Ingrese el apellido paterno: ")
-    Materno = input("Ingrese el apellido materno: ")
-    Direccion = input("Ingrese la dirección: ")
-    Estado = input("Ingrese el estado: ")
-    Ciudad = input("Ingrese la ciudad: ")
-    Nac_str = input("Ingrese la fecha de nacimiento (DD/MM/AAAA): ")
-    Tel = input("Ingrese el numero de telefono: ")
-    C_personal = input("Ingrese el correo personal: ")
-    Matricula = input("Ingrese la matricula: ")
-    C_institucional = input("Ingrese el correo institucional: ")
-    Fac = input("Ingrese la facultad: ")
-    Lic = input("Ingrese la licenciatura: ")
-    F_ingreso_str = input("Ingrese la fecha de ingreso (DD/MM/AAAA): ")
+    while True:
+        Nombres = input("Ingrese el nombre: ")
+
+        if Nombres != "" and Nombres.replace(" ","").isalpha() and len(Nombres) < 2254:
+            break
+        else:
+            print("No puede estar vacío ni contener números.")
+
+    while True:
+        Paterno = input("Ingrese el apellido paterno: ")
+
+        if Paterno != "" and Nombres.replace(" ","").isalpha():
+            break
+        else:
+            print("No puede estar vacío ni contener números.")
+
+    while True:
+        Materno = input("Ingrese el apellido materno: ")
+
+        if Materno != "" and Nombres.replace(" ","").isalpha():
+            break
+        else:
+            print("No puede estar vacío ni contener números.")
+
+    while True:
+        Direccion = input("Ingrese la dirección: ")
+
+        if Direccion != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Estado = input("Ingrese el estado: ")
+
+        if Estado != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Ciudad = input("Ingrese la ciudad: ")
+
+        if Ciudad != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Nac_str = input("Ingrese la fecha de nacimiento (DD/MM/AAAA): ")
+
+        try:
+            Nacimiento = datetime.strptime(Nac_str, "%d/%m/%Y").date()
+            break
+        except ValueError:
+            print("Formato incorrecto. Usa el formato DD/MM/AAAA")
+
+    while True:
+        Tel = input("Ingrese el numero de telefono: ")
+        
+        if Tel.isdigit() and len(Tel) == 10:
+            break
+        else:
+            print("El teléfono debe contener exactamente 10 números.")
+
+    while True:
+        C_personal = input("Ingrese el correo personal: ")
+        if C_personal != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Matricula = input("Ingrese la matricula: ")
+
+        if Matricula != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        C_institucional = input("Ingrese el correo institucional: ")
+
+        if C_institucional != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Fac = input("Ingrese la facultad: ")
+
+        if Fac != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        Lic = input("Ingrese la licenciatura: ")
+
+        if Lic != "":
+            break
+        else:
+            print("No puede estar vacío")
+
+    while True:
+        F_ingreso_str = input("Ingrese la fecha de ingreso (DD/MM/AAAA): ")
+
+        try:
+            F_ingreso = datetime.strptime(F_ingreso_str, "%d/%m/%Y").date()
+            break
+        except ValueError:
+            print("Formato incorrecto. Usa el formato DD/MM/AAAA")
 
     Nacimiento = datetime.strptime(Nac_str, "%d/%m/%Y").date()
     F_ingreso = datetime.strptime(F_ingreso_str, "%d/%m/%Y").date()
 
 
-    nuevo = contacto(Nombres, Paterno, Materno, Direccion, Estado, Ciudad, 
+    nuevo_contacto = contacto(Nombres, Paterno, Materno, Direccion, Estado, Ciudad, 
                     Nacimiento, Tel, C_personal, Matricula,
                     C_institucional, Fac, Lic, F_ingreso)
 
-    lista_contactos.insertar(nuevo)
+    lista_contactos.insertar(nuevo_contacto)
 
     print("\nContacto guardado con éxito.")
     input("Presiona Enter para continuar...")
