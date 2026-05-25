1fa0a04455358344835d0382e9f45202bc6e6c28
Ilse <ilsemartinezchimal@gmail.com>
2026-05-24 19:41:36 -0600

    actualizando

--- hash.py
+++
@@ -1,8 +0,0 @@
-_PRIMO = 31
-_MODULO = 100000000
-
-def generar_hash(matricula):
-    acumulador = 7
-    for caracter in matricula:
-        acumulador = (acumulador * _PRIMO + ord(caracter)) % _MODULO
-    return abs(acumulador) + 1 #para evitar el cero
